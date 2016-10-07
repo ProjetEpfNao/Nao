77886f7013f60f92efd2906fb0a2b086af522780
EtienneDesticourt <etienne.desticourt@gmail.com>
2016-10-07 17:45:30 +0200

    Added first tests for client

--- client/client.py
+++ client/client.py
@@ -1,17 +1,23 @@
 from password_manager import PasswordManager
 from client_exception import ConnectionError
 import rest_api
-import requests
 
 
 class Client(object):
     "Object in charge of communicating to the server, sending status info and fetching commands."
 
-    def __init__(self):
+    def __init__(self, Session):
         "Creates a new client and attempts to authenticate to the server."
-        self.session = requests.Session()
+        self.session = Session()
+        self.password_manager = None
 
-        # Create a password manager to perform auth
+    def start(self):
+        "Loads credentials and authenticates with the server."
+        self.init_password_manager()
+        self.authenticate()
+
+    def init_password_manager(self):
+        "Creates a password manager and loads or fetches credentials."
         self.password_manager = PasswordManager()
         try:
             self.password_manager.load()
@@ -19,28 +25,17 @@ class Client(object):
             user, passwd = self.fetch_new_credentials()
             self.password_manager.save(user, passwd)
 
-        # Fetches auth url to create auth cookie in session
-        self.authenticate()
-
     def fetch_new_credentials(self):
         "Fetches a new username/password pair from the server, creating a robot object on it by doing so."
         resp = self.session.get(rest_api.CREDENTIALS_URL)
         if resp.status_code != 200:
             raise ConnectionError(resp.status_code)
         data = resp.json()
-        return (data[resp_api.CRED_USER_KEY], data[rest_api.CRED_PASS_KEY])
+        return (data[rest_api.CRED_USER_KEY], data[rest_api.CRED_PASS_KEY])
 
     def authenticate(self):
         pass
 
-    def send_battery_info(self, battery):
-        pass
-
-    def open_command_stream(self):
-        pass
-
-    def send_battery_info(self):
-        pass
-
 if __name__ == "__main__":
-    Client()
+    import requests
+    Client(requests.Session)
---
+++ client/tests/test_client.py
@@ -0,0 +1,61 @@
+from client import client, rest_api, client_exception
+import unittest
+import os
+import requests
+
+TEST_MODE_SUCCESS = "success"
+TEST_MODE_FAILURE = "failure"
+TEST_USER = "test_user"
+TEST_PASS = "test_pass"
+
+
+class MockResponse(object):
+
+    def __init__(self, status_code, json_data):
+        self.status_code = status_code
+        self.json_data = json_data
+
+    def json(self):
+        return self.json_data
+
+
+def mock_get(session, url, *args):
+    if session.test_mode == TEST_MODE_FAILURE:
+        return MockResponse(404, {})
+    if url == rest_api.CREDENTIALS_URL:
+        return MockResponse(200, {rest_api.CRED_USER_KEY: TEST_USER, rest_api.CRED_PASS_KEY: TEST_PASS})
+    return MockResponse(404, {})
+
+
+class TestClient(unittest.TestCase):
+
+    def setUp(self):
+        Session = requests.Session
+        Session.get = mock_get
+        self.c = client.Client(Session)
+        self.c.session.test_mode = TEST_MODE_SUCCESS
+
+    def tearDown(self):
+        del self.c
+
+    def test_init_password_manager(self):
+        self.c.init_password_manager()
+
+        assert self.c.password_manager != None
+        assert self.c.password_manager.username != None
+        assert self.c.password_manager.password != None
+
+    def test_fetch_new_credentials_success(self):
+        user, passwd = self.c.fetch_new_credentials()
+
+        assert user == TEST_USER
+        assert passwd == TEST_PASS
+
+    def test_fetch_new_credentials_bad_status_code(self):
+        self.c.session.test_mode = TEST_MODE_FAILURE
+        self.assertRaises(client_exception.ConnectionError,
+                          self.c.fetch_new_credentials)
+
+
+if __name__ == "__main__":
+    unittest.main()
